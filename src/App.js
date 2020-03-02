afb65a0cf85f583bf04cd62cbbcb75b7272b7db2
zaheersani <sani.zaheer@gmail.com>
2020-03-02 19:46:37 +0500

    CV Components Lab Task Demo

--- src/App.js
+++ src/App.js
@@ -4,24 +4,49 @@ import './App.css';
 
 import CompVar from "./components/MyComponent";
 
-function App() {
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-2">
+            <img src={this.props.pic} width="50px" />
+          </div>
+          <div className="col-6" style={{ fontSize: 24 }}>
+            {this.props.name}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const Information = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <CompVar />
-        <p>
-          I have modified App Component
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Address: {props.data.address}</p>
+      <p>City: {props.data.city}</p>
+      <p>Email: {props.data.email}</p>
+    </div>
+  );
+}
+
+function App() {
+  const info = {
+    address: "H 33, St 99, ParkRoad",
+    city: "Islamabad",
+    phone: "+9234243442",
+    email: "email@email.com"
+  }
+  return (
+    <div className="container">
+      <Header
+        name="Zaheer Sani"
+        pic="https://bmc.edu.pk/wp-content/uploads/2019/03/doctor-red-1.png"
+      />
+      <Information
+        data={info}
+      />
     </div>
   );
 }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -11,6 +11,7 @@ function CompVar() {
         <div>
             <MyComp2 text="Text to be displayed" value={val} />
             <MyComp2 text="2nd Time Called" value={78} />
+            <img src="https://bmc.edu.pk/wp-content/uploads/2019/03/doctor-red-1.png" />
             <h1>This is basic Comp Multiline</h1>
             <h2>This is child Comp</h2>
             <button onClick={() => { alertMe('Called from onClick') }} >Button 1</button>
