cd49572006de155a56671c8e902f65312b9a0657
zaheersani <sani.zaheer@gmail.com>
2020-03-04 09:54:18 +0500

    State and Hooks, Function and Class Based

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import CompVar from "./components/MyComponent";
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: 'Hi'
+    };
+  }
+  updateState = (message) => {
+    if (this.state.msg !== message)
+      this.setState({ msg: message });
+  }
   render() {
+    this.rand = Math.floor(Math.random() * 100);
     return (
       <div className="container">
         <div className="row">
+          <div>
+            <h3>{this.state.msg}</h3>
+            <h5>{this.rand}</h5>
+            <button onClick={() => { this.updateState('Hello') }} >Change State</button>
+          </div>
           <div className="col-2">
             <img src={this.props.pic} width="50px" />
           </div>
@@ -22,8 +38,17 @@ class Header extends React.Component {
 }
 
 const Information = (props) => {
+  const [getMsg, setMsg] = useState('funState Hi');
+  const [getCounter, setCounter] = useState(0);
+  const updateCounter = () => {
+    // ToDo
+    // Write code to update Counter
+  }
   return (
     <div>
+      <h3>State: {getMsg}</h3>
+      <h5>Counter: {getCounter}</h5>
+      <button onClick={() => { setCounter(Math.floor(Math.random() * 100)) }} >New Randome Number</button>
       <p>Address: {props.data.address}</p>
       <p>City: {props.data.city}</p>
       <p>Email: {props.data.email}</p>
